f8ba315903c8887cd0aad995a9c76ef17a9fd48c
mfsalinasz <salinas.programador@gmail.com>
2026-05-21 23:54:47 -0600

    Actualizacion de proyecto para que funcione en Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.memorama.app',
+  appName: 'memorama',
+  webDir: 'www'
+};
+
+export default config;
